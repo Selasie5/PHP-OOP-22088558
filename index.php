dd0dcc9fa85d39fcdf6d1edd9e55e4f4b6b42b82
Selasie5 <selasisepenu5@gmail.com>
2025-02-06 20:52:10 +0000

    Inhertiance completion

--- index.php
+++ index.php
@@ -1,24 +1,49 @@
 <?php
 
-//Class definition for Commission Employee
+//Class definitions
 require_once 'CommissionEmployee.php';
+require_once  "BasePlusCommissionEmployee.php";
 
 //Instance creation of CommissionEmployeeClass
 $employee = new CommissionEmployee("James","Bond","123-45-6789",5000.00,0.1);
+echo "Commission-only Employee: \n";
+$employee->display();
+echo"\n";
 
-echo "Initial Employee Details:\n";
-echo $employee;
 
-//Updating grossSales and commissionRate
-$employee->setGrossSales(10000.00);
-$employee->setCommissionRate(0.5);
+//Instance of  BasePlusCommissionEmployee
+$bpcEmployee = new BasePlusCommissionEmployee("Agnes", "Brown", "135-79-2579",8000.00,0.4,120000);
+echo "Base + Commission Employee: \n";
+$bpcEmployee ->display();
+echo"\n";
+
+
+echo "Calculating Earnings: \n";
+echo "Commission-only Employee: $" . number_format($employee->earnings(),2);
+echo "Base Salary + Commission Employee Earnings: $" . number_format($bpcEmployee->earnings(),2);
+echo "\n";
+//echo "Initial Employee Details:\n";
+//echo $employee;
+
+////Updating grossSales and commissionRate
+//$employee->setGrossSales(10000.00);
+//$employee->setCommissionRate(0.5);
+
+
+echo "Updating Base Salary for BasePlusCommissionEmployee: \n";
+$bpcEmployee -> setBaseSalary(230000);
+echo "Update Base Salary : $" . number_format($bpcEmployee ->getBaseSalary(),2) . "\n";
+
+echo "Updated Earnings: $" . number_format($bpcEmployee ->earnings(),2). "\n";
+
+
 
 //Display updated employee details
-echo "\nUpdated employee details:\n";
-echo "\n-------------------------\n";
-echo $employee;
-
-echo "\n-------------------------\n";
-//Calculate & display earnings
-echo "\nEarnings: $" . number_format($employee->earnings(),2)."\n";
+//echo "\nUpdated employee details:\n";
+//echo "\n-------------------------\n";
+//echo $employee;
+//
+//echo "\n-------------------------\n";
+////Calculate & display earnings
+//echo "\nEarnings: $" . number_format($employee->earnings(),2)."\n";
 
